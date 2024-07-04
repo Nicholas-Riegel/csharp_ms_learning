0139f2fbd8fcde8cd078f0fea3ddff9551f954ab
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-07-04 12:56:58 -0400

    init

---
+++ Classes/Person.cs
@@ -0,0 +1,45 @@
+// namespace Microsoft_videos.Classes
+// {
+//     public class Person
+//     {
+//         public string? FirstName { get; set; }
+//         public string? LastName { get; set;}
+//         public string Name
+//         {
+//             get
+//             {
+//                 return $"{FirstName} {LastName}";
+//             }
+//         }
+
+//         public DateOnly Birthdate { get; set; }
+
+//         public void Introduce(string to)
+//         {
+//             Console.WriteLine("Hi {0}, I am {1}. My birthday is {2}.", to, Name, Birthdate);
+//         }
+//     }
+// }
+
+namespace Microsoft_videos.Classes
+{
+    public class Person(string firstName, string lastName, DateOnly? birthdate = null)
+    {
+        private string? FirstName { get; set; } = firstName;
+        private string? LastName { get; set; } = lastName;
+        private string Name
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        private DateOnly? Birthdate { get; set; } = birthdate;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, I am {Name}." + (Birthdate.HasValue ? $"My birthday is {Birthdate}." : ""));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft_videos.Classes;
+
+// Without constructor
+// Person John = new()
+// {
+//     FirstName = "John",
+//     LastName = "Meresurmizesir"
+// };
+
+// OR
+// var John = new Person()
+// {
+//     FirstName = "John",
+//     LastName = "Meresurmizesir",
+//     Birthdate = new DateOnly(1990, 1, 1)
+// };
+
+// With constructor
+var John = new Person("John", "Meresurmizesir");
+var Mary = new Person("Mary", "Meresurmizesir", new DateOnly(1990, 1, 1));
+
+List<Person> people = [John, Mary];
+
+John.Introduce("Mosh");
